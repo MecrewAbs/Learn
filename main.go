ab7f88ac323057f5de9716370fb3af9b0ec4943a
Andrey <bakrovee@gmail.com>
2025-12-28 23:10:44 +0300

    DZ 7.11

--- main.go
+++ main.go
@@ -12,10 +12,16 @@ func input() {
 	var valChange, valTrans string
 	var valCount string
 	var valNum float32
+	valval := map[string]string{
+		"RUB": "RUB",
+		"USD": "USD",
+		"EUR": "EUR"}
+
 	fmt.Println("Введите валюту : ")
 	fmt.Println("RUB")
 	fmt.Println("USD")
 	fmt.Println("EUR")
+
 	for {
 		fmt.Scan(&valChange)
 		if valChange == "RUB" || valChange == "USD" || valChange == "EUR" {
@@ -24,14 +30,7 @@ func input() {
 			fmt.Println("Попробуйте ещё раз")
 		}
 	}
-	switch {
-	case valChange == "RUB":
-		fmt.Println("RUB")
-	case valChange == "USD":
-		fmt.Println("USD")
-	case valChange == "EUR":
-		fmt.Println("EUR")
-	}
+	fmt.Println(valval[valChange])
 
 	fmt.Println("Введите количество вашей валюты")
 	for {
@@ -46,16 +45,12 @@ func input() {
 	}
 	fmt.Println("You have: ", valCount, valChange)
 	fmt.Println("В какую валюту вы хотите перевести свои деньги?")
-	if valChange != "RUB" {
-		fmt.Println("RUB")
-	}
-	if valChange != "USD" {
-		fmt.Println("USD")
-	}
-	if valChange != "EUR" {
-		fmt.Println("EUR")
-	}
 
+	for key, val := range valval {
+		if valChange != valval[key] {
+			fmt.Println(val)
+		}
+	}
 	for {
 		fmt.Scan(&valTrans)
 		if valTrans == "RUB" || valTrans == "USD" || valTrans == "EUR" {
@@ -64,6 +59,7 @@ func input() {
 			fmt.Println("Некорректное значение")
 		}
 	}
+
 	switch {
 	case valChange == "RUB" && valTrans == "USD":
 		fmt.Println(valNum/80, " USD")
